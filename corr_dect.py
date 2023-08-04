f050a500cc37b22f2936f68ca07fdb8cb44ec798
Kaguya810 <111733414+kaguya810@users.noreply.github.com>
2023-08-05 03:10:10 +0800

    Add files via upload

---
+++ corr_dect.py
@@ -0,0 +1,64 @@
+from machine import Pin
+import sensor, image, time
+import pyb
+import ustruct
+from pyb import UART
+#import seekfree, pyb
+
+# 初始化TFT180屏幕
+#lcd = seekfree.LCD180(3)
+
+# 初始化摄像头
+sensor.reset()
+sensor.set_pixformat(sensor.GRAYSCALE) # 设置图像色彩格式为RGB565格式
+sensor.set_framesize(sensor.QVGA)  # 设置图像大小为160*120
+sensor.set_auto_whitebal(True)      # 设置自动白平衡
+sensor.set_brightness(3000)         # 设置亮度为3000
+sensor.skip_frames(time = 20)       # 跳过帧
+sensor.set_windowing(44,55,200,170)
+clock = time.clock()
+corner = 0
+uart = UART(3, 9600,timeout_char=3000)
+
+while(True):
+    clock.tick()
+    img = sensor.snapshot().lens_corr(0.7).replace(vflip=1,hmirror=1,transpose=0).binary([(0, 39, -11, 20, -14, 20)])
+
+# -----矩形框部分-----
+    # 在图像中寻找矩形
+    for r in img.find_rects(threshold = 10000):
+        # 判断矩形边长是否符合要求
+        if r.w() > 50 and r.h() > 50 and r.w() < 100 and r.h() < 100:
+            # 在屏幕上框出矩形
+            img.draw_rectangle(r.rect(), color = (255, 0, 0), scale = 4)
+            # 获取矩形角点位置
+            corner = r.corners()
+            # 在屏幕上圈出矩形角点
+            sorted(corner,key=(lambda x:x[1]))
+            img.draw_circle(corner[0][0], corner[0][1], 5, color = (0, 0, 255), thickness = 2, fill = False)
+            img.draw_circle(corner[1][0], corner[1][1], 5, color = (0, 0, 255), thickness = 2, fill = False)
+            img.draw_circle(corner[2][0], corner[2][1], 5, color = (0, 0, 255), thickness = 2, fill = False)
+            img.draw_circle(corner[3][0], corner[3][1], 5, color = (0, 0, 255), thickness = 2, fill = False)
+
+
+        # 打印四个角点坐标, 角点1的数组是corner[0], 坐标就是(corner[0][0],corner[0][1])
+        # 角点检测输出的角点排序每次不一定一致，矩形左上的角点有可能是corner0,1,2,3其中一个（几乎为逆时针）
+            corner1_str = f"corner1 = ({corner[0][0]},{corner[0][1]})"
+            corner2_str = f"corner2 = ({corner[1][0]},{corner[1][1]})"
+            corner3_str = f"corner3 = ({corner[2][0]},{corner[2][1]})"
+            corner4_str = f"corner4 = ({corner[3][0]},{corner[3][1]})"
+            print(corner1_str + "\n" + corner2_str + "\n" + corner3_str + "\n" + corner4_str)
+
+
+
+
+        # 按照逆时针遍历，以左下角点为入口
+        # 1，2位返回坐标，3位返回次序
+            FH = bytearray([0x2C,0x12,corner[0][0], corner[0][1],1,1,0x5B])
+            uart.write(FH)
+            FH = bytearray([0x2C,0x12,corner[3][0], corner[3][1],2,1,0x5B])
+            uart.write(FH)
+            FH = bytearray([0x2C,0x12,corner[2][0], corner[2][1],3,1,0x5B])
+            uart.write(FH)
+            FH = bytearray([0x2C,0x12,corner[1][0], corner[1][1],4,1,0x5B])
+            uart.write(FH)
---
+++ point_dect.py
@@ -0,0 +1,76 @@
+import sensor, image, time,pyb,lcd
+import ustruct
+from pyb import UART
+from pid import PID
+#from pyb import Servo
+
+#pan_servo=Servo(1)
+#tilt_servo=Servo(2)
+uart = UART(3, 9600,timeout_char=3000)
+#pan_servo.calibration(500,2500,500)
+#tilt_servo.calibration(500,2500,500)
+
+red_threshold  = (56, 100, 7, 127, 0, 24)
+
+pan_pid = PID(p=0.0732, i=0, imax=90) #脱机运行或者禁用图像传输，使用这个PID
+tilt_pid = PID(p=0.06, i=0, imax=90) #脱机运行或者禁用图像传输，使用这个PID
+#pan_pid = PID(p=0.07, i=0.03, imax=90)#在线调试使用这个PID
+#tilt_pid = PID(p=0.06, i=0.03, imax=90)#在线调试使用这个PID
+RED_LED_PIN = 1
+BLUE_LED_PIN = 3
+lcd.init()
+sensor.reset() # Initialize the camera sensor.
+sensor.set_pixformat(sensor.RGB565) # use RGB565.
+sensor.set_framesize(sensor.QQVGA2) # use QQVGA for speed.
+sensor.skip_frames(10) # Let new settings take affect.
+sensor.set_auto_whitebal(False) # turn this off.
+sensor.set_auto_gain(False) # 颜色跟踪必须关闭自动增益
+clock = time.clock() # Tracks FPS.
+
+def sending_data(cx,cy,cw,ch):
+    global uart;
+    data = ustruct.pack("<bbhhhhb",      #格式为俩个字符俩个短整型(2字节)
+                   0x2C,                      #帧头1
+                   0x12,                      #帧头2
+                   int (cx), # up sample by 4    #数据26
+                   int (cy),
+                   1,
+                   1,
+                   0x5B)
+    uart.write(data);   #必须要传入一个字节数组
+def find_max(blobs):
+    max_size=0
+    for blob in blobs:
+        if blob[2]*blob[3] > max_size:
+            max_blob=blob
+            max_size = blob[2]*blob[3]
+    return max_blob
+
+
+while(True):
+    pyb.LED(RED_LED_PIN).on()
+    clock.tick() # Track elapsed milliseconds between snapshots().
+    img = sensor.snapshot().replace(vflip=1,hmirror=1,transpose=0) # Take a picture and return the image.
+    blobs = img.find_blobs([red_threshold])
+    if blobs:
+        max_blob = find_max(blobs)
+        pan_error = max_blob.cx()-img.width()/2
+        tilt_error = max_blob.cy()-img.height()/2
+        #print("pan_error: ", pan_error
+        img.draw_rectangle(max_blob.rect()) # rect
+        img.draw_cross(max_blob.cx(), max_blob.cy()) # cx, cy
+        pan_output=pan_pid.get_pid(pan_error,1)/2
+        tilt_output=tilt_pid.get_pid(tilt_error,1)
+        print("pan_output",pan_output)
+        print("tilt_output",tilt_output)
+        #pan_servo.angle(pan_servo.angle()+pan_output)
+        #tilt_servo.angle(tilt_servo.angle()-tilt_output)
+        cx=(int)(pan_output*10)
+        cy=(int)(tilt_output*10)
+        print(cx)
+        print(cy)
+        FH=bytearray([0x2C,0x12,cx,cy,1,1,0x5B])
+        uart.write(FH)
+        cx=0
+        cy=0
+
